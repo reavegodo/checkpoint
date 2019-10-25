73fd50d69d0a8adc094e5a41bca408c7bdb233b0
reavegodo <56036113+reavegodo@users.noreply.github.com>
2019-10-25 09:57:59 +0200

    Update Student.java

--- algos/Student.java
+++ algos/Student.java
@@ -1,10 +1,27 @@
 public class Student {
-
     // TODO hasLegalAge
+    public static boolean hasLegalAge(int age) {
+        return age >= 18;
+    }
 
     // TODO getGroup
+    public static char getGroup(int num) {
+        if (num % 2 == 0) {
+            return 'A';
+        }
+        return 'B';
+    }
 
     // TODO countStudents
+    public static int countStudents(String[] tableau) {
+        int count = 0;
+        for (String language : tableau) {
+            if (language.equalsIgnoreCase("java")) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
 
 
